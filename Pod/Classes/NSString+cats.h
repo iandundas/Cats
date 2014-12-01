18922b4867824187f1313579d2d3ffc38ab00bca
Ian Dundas <contact@iandundas.co.uk>
2014-12-01 14:46:09 +0100

    Added a few basic placeholders

---
+++ Pod/Classes/NSString+cats.h
@@ -0,0 +1,8 @@
+//
+// Created by Ian Dundas on 01/12/14.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (cats)
+@end
---
+++ Pod/Classes/NSString+cats.m
@@ -0,0 +1,8 @@
+//
+// Created by Ian Dundas on 01/12/14.
+//
+
+#import "NSString+cats.h"
+
+@implementation NSString (cats)
+@end
